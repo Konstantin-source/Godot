6f0ed0aab8c116c09f785864016ed681a2766644
Konsti23424 <konstantinmuschallik@gmail.com>
2025-01-27 21:57:01 +0100

    neues

---
+++ tank_1.gd
@@ -0,0 +1,25 @@
+extends RigidBody2D
+var velocity = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	var direction = Vector2()
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("down"):
+		direction.y += 1
+	if Input.is_action_pressed("up"):
+		direction.y -= 1
+	direction = direction.normalized()
+	velocity = direction * velocity
+	velocity = move_and_slide(velocity)
